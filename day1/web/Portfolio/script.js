e00fe68bede5c5281f9c03c53cf75618f9a0d7c1
NLescaut <nathan.lescaut@epitech>
2026-02-02 17:23:40 +0100

    task1-3

---
+++ day1/web/Portfolio/script.js
@@ -0,0 +1,14 @@
+document.getElementById('E-todo').addEventListener('click', () => {
+    console.log('Button clicked!');
+    alert("https://github.com/EpitechBachelorPromo2028/B-WEB-101-PAR-1-1-etodo-14.git");
+});
+
+document.getElementById('hack_juice').addEventListener('click', () => {
+    console.log('Button clicked!');
+    alert('vous allez voir mon github hack_juice jai travailler avec Burp');
+});
+
+document.getElementById('Yowl').addEventListener('click', () => {
+    console.log('Button clicked!');
+    alert('vous allez voir mon github Yowl jai travailler avec du HTML et Figma');
+});
